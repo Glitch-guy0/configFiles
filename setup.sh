168af5b74e22fc9e835382cd961cfdbad3627c39
prajwal <prajwalm9845@gmail.com>
2023-05-25 21:26:29 +0530

    adding fonts for configs

--- setup.sh
+++ setup.sh
@@ -5,14 +5,21 @@ apt upgrade -y;
 
 # crul installation
 apt install curl -y;
+apt install zip -y;
 
 # starship installation 
-curl -sS https://starship.rs/install.sh | sh;
+curl -sS https://starship.rs/install.sh | sh -y;
 
 # placing invoking script to .bashrc
 cat initCommand.insert >> ~/.bashrc;
+
+mkdir ~/.fonts
+cp JetBrainsMono.zip ~/.fonts
+unzip JetBrainsMono.zip
+fc-cache -fv
+
 # placing my config file into config folder
-mkdir ~/.config
+mkdir ~/.config 2> /dev/NULL
 touch ~/.config/starship.toml
 cat configuration.insert > ~/.config/starship.toml
 
